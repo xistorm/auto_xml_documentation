29e7528f6f39e77a19e0d652abea2811c66ef556
xistorm <xis402@gmail.com>
2024-05-25 17:31:54 +0300

    add logic and api calls to plugin

---
+++ plugin/src/api/documentation.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+
+dotenv.config();
+
+export class DocumentationAPI {
+    private static HOST = process.env.API_HOST || 'localhost';
+    private static PORT = process.env.API_PORT || 8000;
+
+    private static buildUrl = (endpoint: string) => {
+        return `http://${this.HOST}:${this.PORT}/api/documentation/${endpoint}`
+    };
+
+    static create = async (code_text: string, language: string = 'ru') => {
+        const url = this.buildUrl('create');
+        const data = { code_text, language };
+
+        const response = await axios.post(url, data);
+        const documented_text = await response.data;
+
+        return documented_text;
+    };
+}
--- plugin/src/controllers/documentation.ts
+++ plugin/src/controllers/documentation.ts
@@ -1,21 +1,24 @@
 import * as vscode from 'vscode';
 
+import { DocumentationAPI } from '../api';
+
 
 export class DocumentationController {
     static createDocumentation = (language: string) => {
-        const createLanguageDocumentation = (): void => {
+        const createLanguageDocumentation = async () => {
             const editor = vscode.window.activeTextEditor;
-
-            if (editor) {
-                const document = editor.document;
-                const selection = editor.selection;
-
-                const word = document.getText(selection);
-                const reversed = word.split('').reverse().join('');
-                editor.edit(editBuilder => {
-                    editBuilder.replace(selection, reversed);
-                });
+            if (!editor) {
+                return;
             }
+
+            const document = editor.document;
+            const selection = editor.selection;
+
+            const text = document.getText(selection);
+            const documented_text = await DocumentationAPI.create(text, language);
+            editor.edit(editBuilder => {
+                editBuilder.replace(selection, documented_text);
+            });
         };
 
         return createLanguageDocumentation;
